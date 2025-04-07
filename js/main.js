0dfa933289dd260d15b6c6c5dc6df841cb4909e8
Lav-P365 <365lavp@gmail.com>
2025-04-07 13:28:57 -0400

    Update Home page

--- js/main.js
+++ js/main.js
@@ -1,55 +1,53 @@
 const app = Vue.createApp({
-    created() {
-        // Ideal to get your remote data during the created phase
-        fetch('http://localhost/lumen-backend/public/teams')
-            .then(res => res.json())
-            .then(data => {
-              this.booksData = data
-              this.loadingBooks = false
-            })
-            .catch(error => console.error(error));
-    },
-    data() {
-        return {
-            teams: [],
-            name: "",
-            published_date: "",
-            bio: "",
-            image: "",
-            error: "",
-            loading: false,
-            loadingTTeams: true,
-        }
-    },
+  created() {
+    // Fetch all IPL teams
+    fetch('http://localhost/lumen-backend/public/teams')
+      .then(res => res.json())
+      .then(data => {
+        this.teamsData = data;
+        this.loadingTeams = false;
+      })
+      .catch(error => {
+        this.error = "Unable to fetch teams.";
+        this.loadingTeams = false;
+      });
+  },
+  data() {
+    return {
+      teamsData: [],
+      owner: "",
+      founded: "",
+      stadium: "",
+      image: "",
+      error: "",
+      loadingTeams: true,
+      loadingDetails: false,
+    };
+  },
+  methods: {
+    getTeamDetails(id) {
+      this.loadingDetails = true;
+      this.error = "";
 
-    methods: {
-      getTeams(id) {
-        console.log(id)
-        this.loading = true;
-        this.error = false;
-        const teamInfoCon = document.querySelector("#teamInfoCon");
-
-        fetch(`http://localhost:8888/bookstore-api/public/books/${id}`)
-            .then(res => res.json())
-            .then(data => {
-              if (data.length > 0) {
-              console.log(data[0]);  
-              const bookData = data[0];
-              this.loading = false; 
-              const bookInfoCon = document.querySelector("#bookInfoCon");          
-              // This is using the ternary operator (condition ? value_if_true : value_if_false), which is a shorthand for an if-else statement.
-              this.name = bookData.name ? bookData.name : 'Not available';
-              this.published_date = bookData.published_date ? bookData.published_date : 'Not available';
-              this.bio = bookData.bio ? bookData.bio : 'Not available';
-              this.image = bookData.book_image ? bookData.book_image : 'Not available';
-              } else {
-                this.loading = false;
-                this.error = 'No book found with the given query';
-              }
-          }).then(() => {
-            bookInfoCon.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      fetch(`http://localhost/lumen-backend/public/teams/${id}`)
+        .then(res => res.json())
+        .then(data => {
+          if (data.length > 0) {
+            const team = data[0];
+            this.owner = team.owner || "Not available";
+            this.founded = team.founded || "Not available";
+            this.stadium = team.stadium || "Not available";
+            this.image = team.image || "placeholder.jpg";
+          } else {
+            this.error = "No details found.";
+          }
+          this.loadingDetails = false;
+          document.querySelector("#teamInfoCon").scrollIntoView({ behavior: "smooth", block: "end" });
         })
-          .catch(error => console.error(error));
-      }
+        .catch(error => {
+          this.error = "Failed to load team details.";
+          this.loadingDetails = false;
+        });
     }
+  }
 }).mount('#app');
